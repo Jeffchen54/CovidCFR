04878aeae76d44c7e1d0b4be4f07ae97544f4e33
az1zabousam <az1zabousam@vt.edu>
2020-11-16 15:53:20 -0500

    Test for Race Class

---
+++ CovidCFR/src/prj5/RaceTest.java
@@ -0,0 +1,49 @@
+/**
+ * “I have neither given nor received unauthorized assistance on this
+ * assignment.” - AA
+ */
+
+package prj5;
+
+import student.TestCase;
+
+/**
+ * Ensure methods within the Race class are working as intended
+ * 
+ * @author Aziz Abousam (az1zabousam)
+ * @version 2020.16.11
+ */
+public class RaceTest extends TestCase {
+    private Race race;
+    
+    /**
+     * The setUp method is used to initialize initial
+     * conditions and will run before every test method
+     */
+    public void setUp() {
+        race = new Race("black", 179563, 13365);
+    }
+    
+    /**
+     * Ensure the private helper method calcCFR(int, int) used
+     * within the constructor is correctly calculating and 
+     * setting the CFR
+     */
+    public void testRace() {
+        assertEquals(7.44, race.getCFR(), 0.01);
+    }
+    
+    /**
+     * Ensure getName() returns the correct name of the race
+     */
+    public void testGetName() {
+        assertEquals("black", race.getName());
+    }
+    
+    /**
+     * Ensure getCFR() returns the CFR for the race
+     */
+    public void testGetCFR() {
+        assertEquals(7.44, race.getCFR(), 0.01);
+    }
+}
