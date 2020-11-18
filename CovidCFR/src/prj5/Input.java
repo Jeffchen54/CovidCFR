4b4458f73b22307d33b34356b7888974bd1424ca
aalbro <aalbro@vt.edu>
2020-11-18 18:25:29 -0500

    Input class

---
+++ CovidCFR/src/prj5/Input.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package prj5;
+
+/**
+ * @author Andrew Albro aalbro
+ * @version 2020.11.18
+ *
+ */
+public class Input
+{
+    public Input()
+    {
+        
+    }
+    
+    public static void main(String[] file)
+    {
+        
+    }
+}
