f9a526503b9ec26094144d3e9893696541fbef96
aalbro <aalbro@vt.edu>
2020-11-18 00:49:17 -0500

    updated stateTest class

--- CovidCFR/src/prj5/StateTest.java
+++ CovidCFR/src/prj5/StateTest.java
@@ -2,14 +2,7 @@
   *  assistance on this assignment.” - AA
   *  */
 package prj5;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.lang.reflect.Array;
-
-import org.junit.jupiter.api.Test;
-
-import junit.framework.TestCase;
+import student.TestCase;
 
 /**
  * @author Andrew Albro aalbro
@@ -18,8 +11,19 @@ import junit.framework.TestCase;
  */
 public class StateTest extends TestCase
 {
-    private Race races;
+    private Race[] races;
     private State state;
+    
+    public void setUp()
+    {
+        races = new Race[5];
+        races[4] = new Race("asian", 5407, 254);
+        races[3] = new Race("black", 179563, 13365);
+        races[0] = new Race("latino", 97118, 2269);
+        races[1] = new Race("other", 108784, 170);
+        races[2] = new Race("white", 70678, 1924);
+        state = new State("DC", races);
+    }
     
     /**
      * This tests the sortAlpha method.
@@ -28,7 +32,9 @@ public class StateTest extends TestCase
     {
         state.sortAlpha();
         assertEquals(state.getNumberOfEntries(), 5);
-        assertEquals(state.getFront(), "asian");
+        assertEquals(state.toString(), "DC\nasian: 5407 cases, 4.7% CFR\n"
+            + "black: 179563 cases, 7.4% CFR\nlatino: 97118 cases, 2.3% CFR\n"
+            + "other: 108784 cases, 0.2% CFR\nwhite: 70678 cases, 2.7% CFR");
     }
     
     /**
@@ -38,19 +44,9 @@ public class StateTest extends TestCase
     {
         state.sortCFR();
         assertEquals(state.getNumberOfEntries(), 5);
-        assertEquals(state.getFront(), "black");    
-    }
-    
-    /**
-     * This tests the toString method.
-     */
-    public void testToString()
-    {
-        races = new Race("VA", 100, 10);
-        state = new State("VA", races);   
-        assertEquals(state.toString(), "white" + ":" + " "
-            + 100 + " " + "cases" + "," + " " + "10.0%" 
-            + " " + "CFR");
+        assertEquals(state.toString(), "DC\nblack: 179563 cases, 7.4% CFR\n"
+            + "asian: 5407 cases, 4.7% CFR\nwhite: 70678 cases, 2.7% CFR\n"
+            + "latino: 97118 cases, 2.3% CFR\nother: 108784 cases, 0.2% CFR");
     }
     
 
