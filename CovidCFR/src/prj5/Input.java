edb141b7d31371d4bb10e7bbb685f5a5951875e8
aalbro <aalbro@vt.edu>
2020-11-18 18:31:50 -0500

    Input class

--- CovidCFR/src/prj5/Input.java
+++ CovidCFR/src/prj5/Input.java
@@ -1,7 +1,10 @@
 /**
- * 
+ * “I have neither given nor received unauthorized assistance on this
+ * assignment.” - ARA
  */
 package prj5;
+
+import java.io.FileNotFoundException;
 
 /**
  * @author Andrew Albro aalbro
@@ -12,11 +15,43 @@ public class Input
 {
     public Input()
     {
-        
+        //nothing needed here               
     }
     
-    public static void main(String[] file)
+    /**
+     * This method reads the file and correctly
+     * outputs the files information.
+     * @param fileName the fileName of the input file.
+     * @throws FileNotFoundException if the file isn't found.
+     */
+    public static void main(String[] fileName) throws FileNotFoundException
     {
+        CovidReader read = new CovidReader("filename"); 
+        State[] states = read.getAllStates();
+        states[0].sortAlpha();
+        System.out.println("=====\n");
+        states[0].sortCFR();
+        System.out.println("=====\n");
+        states[1].sortAlpha();
+        System.out.println("=====\n");
+        states[1].sortCFR();
+        System.out.println("=====\n");
+        states[2].sortAlpha();
+        System.out.println("=====\n");
+        states[2].sortCFR();
+        System.out.println("=====\n");
+        states[3].sortAlpha();
+        System.out.println("=====\n");
+        states[3].sortCFR();
+        System.out.println("=====\n");
+        states[4].sortAlpha();
+        System.out.println("=====\n");
+        states[4].sortCFR();
+        System.out.println("=====\n");
+        states[5].sortAlpha();
+        System.out.println("=====\n");
+        states[5].sortCFR();
+        System.out.println("=====\n");
         
     }
 }
