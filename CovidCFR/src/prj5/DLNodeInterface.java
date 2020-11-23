c2ba0d0470cbf49ba48c5c92d082c270990a09f7
aalbro <aalbro@vt.edu>
2020-11-23 13:44:45 -0500

    DLNodeInterface class updated

--- CovidCFR/src/prj5/DLNodeInterface.java
+++ CovidCFR/src/prj5/DLNodeInterface.java
@@ -1,6 +1,6 @@
 /**
  * “I have neither given nor received unauthorized assistance on this
- * assignment.” -
+ * assignment.” -AA
  */
 
 package prj5;
@@ -9,11 +9,49 @@ package prj5;
 /**
  * TODO Fill the blanks and add initials to honor code
  * 
- * @author Aziz Abousam (az1zabousam)
+ * @author Andrew Albro (aalbro)
+ * @version 2020.11.23
  * @param <T>
  *            Type of data stored in objects implementing this interface.
  * @version 11.14.2020
  */
 
 public interface DLNodeInterface<T> {
+    
+    /**
+     * This returns the data of the node.
+     * @return the data of the node.
+     * @Override
+     */
+    public T getData();
+    
+    /**
+     * This gets the next node.
+     * @return the next node.
+     * @Override
+     */
+    public DLNode<T> getNextNode();
+    
+    /**
+     * This sets the next node
+     * of the list.
+     * @param node the parameter to 
+     * set the next node too.
+     */
+    public void setNextNode(DLNode<T> node);
+    
+    /**
+     * This sets the current node to
+     * the previous node.
+     * @return the previous node.
+     * @Override
+     */
+    public DLNode<T> setPrevNode();
+    
+    /**
+     * This gets the previous node 
+     * and returns it.
+     * @return the previous node.
+     */
+    public DLNode<T> getPrevNode();
 }
