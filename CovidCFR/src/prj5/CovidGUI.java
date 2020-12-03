eb9ca0e7993c3c25ff4cd3b2395ba98f24a599e1
chenjeff4840 <chenjeff4840@vt.edu>
2020-12-02 19:23:04 -0500

    Finalized functionality and some comments for GUI

--- CovidCFR/src/prj5/CovidGUI.java
+++ CovidCFR/src/prj5/CovidGUI.java
@@ -46,18 +46,13 @@ public class CovidGUI {
      * This constructor will create the window and give functionality to the
      * buttons.
      * 
-     * @apiNote Removed hardcoded segments, added commits to remove
      * @param states1
      *            States data to be visualized within the window
      */
     public CovidGUI(State[] states1) {
-
-        // TODO Remove these // comments and apiNote
-        // Initial setup
         states = states1;
         window = new Window();
 
-        // Window, alpha, cfr button created
         window.setTitle("Covid Fatality by Race");
         sortAlpha = new Button("Sort by Alpha");
         sortAlpha.onClick(this, "clickedSortAlpha");
@@ -65,7 +60,6 @@ public class CovidGUI {
         quit = new Button("Quit");
         quit.onClick(this, "clickedQuit");
 
-        // State buttons
         window.addButton(quit, WindowSide.NORTH);
         sortCFR = new Button("Sort by CFR");
         sortCFR.onClick(this, "clickedSortCFR");
@@ -89,13 +83,12 @@ public class CovidGUI {
         state6.onClick(this, "clickedState");
         window.addButton(state6, WindowSide.SOUTH);
 
-        // State buttons registered
         stateButtons = new Button[] { state1, state2, state3, state4, state5,
             state6 };
-        
+
         cfrMode = false;
         alphaMode = false;
-        
+
     }
 
     // Methods .................................................................
@@ -115,27 +108,23 @@ public class CovidGUI {
 
     /**
      * This method will sort the bar graphs by the names
-     * of the races' in alphabetical order from left to right
+     * of the races' in alphabetical order from left to right. Does nothing
+     * if no state has been selected yet.
+     * 
+     * @param button
+     *            button used to signal the sorting of races into alpha order.
      */
     public void clickedSortAlpha(Button button) {
-        
-        cfrMode = false;
-        alphaMode = true;
-        
+
         if (title == null) {
             return;
         }
-        String stateName = title.getText().substring(0, 2);
-        State state = null;
-        for (State state1 : states) {
-            if (state1.getStateName().equals(stateName)) {
-                state = state1;
-                break;
-            }
-        }
 
-        state.sortAlpha();
-        
+        cfrMode = false;
+        alphaMode = true;
+
+        String stateName = title.getText().substring(0, 2);
+
         for (Button stateButton : stateButtons) {
             if (stateButton.getTitle().substring(10).equals(stateName)) {
                 clickedState(stateButton);
@@ -147,32 +136,22 @@ public class CovidGUI {
 
     /**
      * This method will sort the bar graphs by CFR
-     * in descending order from left to right
+     * in descending order from left to right. Does nothing if no state is
+     * currently selected.
+     * 
+     * @param button
+     *            button used to signal sorting into CFR order.
      */
     public void clickedSortCFR(Button button) {
-        
-        cfrMode = true;
-        alphaMode = false;
-        
-        // Checks if no state is selected
         if (title == null) {
             return;
         }
-        
-        // Grabs the state
-        String stateName = title.getText().substring(0, 2);
-        State state = null;
-        for (State state1 : states) {
-            if (state1.getStateName().equals(stateName)) {
-                state = state1;
-                break;
-            }
-        }
-        
-        // This sorts the state, it returns an iterator but not needed 
-        state.sortCFR();
 
-        // Calls clickedState on the correct button.
+        cfrMode = true;
+        alphaMode = false;
+
+        String stateName = title.getText().substring(0, 2);
+
         for (Button stateButton : stateButtons) {
             if (stateButton.getTitle().substring(10).equals(stateName)) {
                 clickedState(stateButton);
@@ -186,14 +165,9 @@ public class CovidGUI {
      * Mapped to buttons correlating to states. Display COVID data onto the GUI.
      * 
      * @param button
-     *              Button which is associated with a specific state
-     * @apiNote Edited to use an iterator to improve efficiency
-     * @apiNote Edited to save alpha or cfr preference.
+     *            Button which is associated with a specific state
      */
     public void clickedState(Button button) {
-        //TODO remove // comments and apiNote
-        
-        // Removes and refreshes constant shapes
         window.removeAllShapes();
         String stateName = button.getTitle().substring(10);
         title = new TextShape(0, 0, stateName + " Case Fatality Ratio by Race");
@@ -201,48 +175,36 @@ public class CovidGUI {
             15);
         window.addShape(title);
 
-        // Finds the correct state
-        State state = null;
-        for (State state1 : states) {
-            if (state1.getStateName().equals(stateName)) {
-                state = state1;
-                break;
-            }
-        }
+        State state = stateSearch(stateName);
 
-        // Puts the COVID Race bars onto the GUI 1 by 1 with an iterator
-        
         Iterator<Race> iter;
-        if(cfrMode) {
+        if (cfrMode) {
             iter = state.sortCFR();
         }
-        else if(alphaMode) {
+        else if (alphaMode) {
             iter = state.sortAlpha();
         }
         else {
             iter = state.getIterator();
         }
-        
+
         for (int i = 0; i < 5; i++) {
-            
-            // Pulls CFR data without removing
+
             Race race = iter.next();
             TextShape raceName = new TextShape(0, 0, race.getName());
             raceName.moveTo(i * 125 + 90, 220);
             window.addShape(raceName);
             double cfr = race.getCFR();
-            
+
             TextShape cfrText;
-            
-            // Sets CFR to N/A if needed
+
             if (cfr == -1) {
                 cfrText = new TextShape(0, 0, "N/A");
                 cfrText.moveTo(i * 125 + 88, 240);
                 window.addShape(cfrText);
                 continue;
             }
-            
-            // Creates and displays the cfr bar and data on the GUI
+
             DecimalFormat df = new DecimalFormat("#.#");
             cfrText = new TextShape(0, 0, df.format(cfr).toString() + "%");
             cfrText.moveTo(i * 125 + 88, 240);
@@ -252,5 +214,25 @@ public class CovidGUI {
             bar.moveTo(i * 125 + 100, 215 - barHeight);
             window.addShape(bar);
         }
+    }
+
+
+    /**
+     * Searches and returns the state whose name matches the String input.
+     * Throws an IllegalArgumentException if the input does not match a
+     * any state.
+     * 
+     * @param stateName
+     *            Name of the State to return.
+     * @return State which corresponds to the inputed stateName
+     * @throws IllgalArgumentException
+     */
+    private State stateSearch(String stateName) {
+        for (State search : states) {
+            if (search.getStateName().equals(stateName)) {
+                return search;
+            }
+        }
+        throw new IllegalArgumentException();
     }
 }
